1557002414d09d71987c9981f9b90110e14b69b3
Phuttimatepra <55490102+Phuttimatepra@users.noreply.github.com>
2020-03-08 01:07:11 +0700

    connect bootstrap 4.4
    import bootstrap by CDN

--- WebBlogUI/src/app/app.module.ts
+++ WebBlogUI/src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  // bootstrap: [AppComponent]
+  bootstrap: [SignInComponent]
 })
 export class AppModule { }
